31fa1284ee2d59d8a72ecbf7bace2fcb9e4dd9ef
CryptocoderAS <as7663@srmist.edu.in>
2021-07-16 11:21:40 +0530

    files added

---
+++ README.md
@@ -0,0 +1,12 @@
+# DICE
+
+A simple flutter dice app. <br>
+It changes the state (numbers) on being pressed.<br>
+
+## APP PREVIEW
+<p >
+  <img src="./images/1.png" width= "420" alt="accessibility text">
+  <img src="./images/2.png" width= "420" title="hover text">
+ 
+</p>
+ 
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text('Dicee')),
+          backgroundColor: Colors.green[900],
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+          //Get students to create the second die as a challenge
+          Expanded(
+            child: FlatButton(
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
